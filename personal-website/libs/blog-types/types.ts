c94b7bdbf30eca103ed182bf80ae4d3d088fe04b
nicholas-yong <40938341+nicholas-yong@users.noreply.github.com>
2022-01-08 09:24:08 +0800

    Commit
    Commit as required

--- personal-website/libs/blog-types/types.ts
+++ personal-website/libs/blog-types/types.ts
@@ -1,9 +1,36 @@
-export interface BlogItem {
+export type BlogTags = "food" | "coding" | "stories" | "general" | "anime"
+
+export interface BlogItemDTO {
 	id: number
+	rev: string
 	title: string
-	mainPicture: string // url of the main picture to serve
+	mainPicture: string
 	teaser: string
-	publicationDate: number // unix timestamp in numbers
+	publicationDate: number // unix timestamp
+	content: string
+	tags: Array<BlogTags>
 }
 
-export type BlogTags = "food" | "coding" | "stories" | "general" | "anime"
+export interface CreateBlogItemDTO {
+	title: string
+	mainPicture: string
+	teaser: string
+	content: string
+	tags: Array<BlogTags>
+}
+
+export interface GetBlogItemDTO {
+	id: number
+	title: string
+	mainPicture: string
+	teaser: string
+	publicationDate: number // unix timestamp'
+	content: string
+	tags: Array<BlogTags>
+}
+
+export type UpdateBlogItemDTO = CreateBlogItemDTO
+
+export interface DeleteBlogItemDTO {
+	id: number
+}
